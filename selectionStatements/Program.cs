27ae06f2f0bb0d58f85507ba1e31d733aa6c5865
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-02 14:33:18 -0400

    if else statements

---
+++ selectionStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace selectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if else statement
+
+            Console.WriteLine("Input numbers 1-50 below to guess my favorite number.");
+            var favNumber = int.Parse(Console.ReadLine());
+
+            if (favNumber <= 6)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (favNumber >= 8)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("You nailed it!");
+            }
+
+        }
+    }
+}
+
